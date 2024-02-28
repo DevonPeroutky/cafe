fe8c7503248d1e474d5644cc10327097b6e519ac
Devon Peroutky <devonperoutky@Devons-Mac-mini.local>
2024-02-27 17:21:44 -0800

    Hit https endpoint in prod

--- src/data/client/constants.ts
+++ src/data/client/constants.ts
@@ -1,4 +1,5 @@
 
 export const LOCAL_BASE_URL = 'http://localhost:8000';
-export const PROD_BASE_URL = 'http://34.146.237.185:8000';
+// export const PROD_BASE_URL = 'http://34.146.237.185:8000';
+export const PROD_BASE_URL = 'https://34.146.237.185:8000';
 export const API_ENDPOINT = PROD_BASE_URL;
