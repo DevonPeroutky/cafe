be015b278e4bccccc2aa3cec34165d2a40b0e274
Devon Peroutky <devonperoutky@Devons-Mac-mini.local>
2024-03-08 19:34:55 -0800

    Update url

--- src/app_components/chat/Placeholder.tsx
+++ src/app_components/chat/Placeholder.tsx
@@ -6,9 +6,8 @@ export const Placeholder = () => {
   return (
       <section className="flex flex-col flex-grow py-8">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Sup
-            Douchebag</h1>
-          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-24 dark:text-gray-400">Most communication is non-verbal. So how good can A.I. therapy if they can't see you. I'm not like other LLMs. I see you and take in your visual cues to make the response more "human" and tailored to you.</p>
+          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Sup Douchebag</h1>
+          <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-24 dark:text-gray-400">Most communication is non-verbal. So how good can A.I. therapy if they can't see you?<br/><br/> I'm not like other LLMs. I see you and take in your visual cues to make the response more "human" and tailored to you.</p>
         </div>
         <div className="flex justify-center mx-auto text-center">
           <div className="text-center list-none p-0 flex flex-col gap-y-3">
--- src/data/client/constants.ts
+++ src/data/client/constants.ts
@@ -1,5 +1,7 @@
+// PROD
+export const PROD_BASE_URL = 'https://34.146.237.185:8000';
 
-export const LOCAL_BASE_URL = 'http://localhost:8000';
+// LOCAL
 // export const PROD_BASE_URL = 'http://34.146.237.185:8000';
-export const PROD_BASE_URL = 'http://34.146.237.185:8000';
+
 export const API_ENDPOINT = PROD_BASE_URL;
